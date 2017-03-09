0a31f9bc75c6c053fdeedd1f569e60ed2c5e5be6
Hanbin Sock <hbsock@hotmail.com>
2017-03-09 13:23:45 -0500

    Initial commit for helpful C++ code samples.

---
+++ CsvParser.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+class CSVRow
+{
+public:
+	std::string const& operator[](std::size_t index) const
+	{
+		return m_data[index];
+	}
+	std::size_t size() const
+	{
+		return m_data.size();
+	}
+	void readNextRow(std::istream& str)
+	{
+		std::string         line;
+		std::getline(str, line);
+
+		std::stringstream   lineStream(line);
+		std::string         cell;
+
+		m_data.clear();
+		while (std::getline(lineStream, cell, ','))
+		{
+			m_data.push_back(cell);
+		}
+		// This checks for a trailing comma with no data after it.
+		if (!lineStream && cell.empty())
+		{
+			// If there was a trailing comma then add an empty element.
+			m_data.push_back("");
+		}
+	}
+private:
+	std::vector<std::string>    m_data;
+};
+
+std::istream& operator >> (std::istream& str, CSVRow& data)
+{
+	data.readNextRow(str);
+	return str;
+}
+
+int main()
+{
+	std::ifstream input("test.txt");
+	
+	CSVRow row;
+	while(input >> row)
+	{
+		for(std::size_t i=0; i < row.size(); ++i)
+		{
+			// row[i] gives parsed csv;
+		}
+	}
+}
